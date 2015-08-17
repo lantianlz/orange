3a8c239b0c1f432ab1235e3d6777c4150eeb692c
stranger <286394973@qq.com>
2015-08-17 14:56:50 +0800

    发送提醒

--- www/company/interface.py
+++ www/company/interface.py
@@ -782,14 +782,16 @@ class CashRecordBase(object):
         async_send_email("vip@3-10.cc", title, content)
 
         # 发送微信提醒
-        for manager in CompanyManager().get_managers_by_company(company.id):
+        for manager in CompanyManagerBase().get_managers_by_company(company.id):
             
             to_user_openid = ExternalTokenBase().get_weixin_openid_by_user_id(manager.user_id)
-            wb.send_balance_insufficient_template_msg(
-                to_user_openid, u"账户已达「%s」元最高透支额，请联系充值" % max_overdraft, 
-                company.name, balance + u" 元", 
-                u"感谢您的支持，祝工作愉快"
-            )
+
+            if to_user_openid:
+                wb.send_balance_insufficient_template_msg(
+                    to_user_openid, u"账户已达「%s」元最高透支额，请联系充值" % max_overdraft, 
+                    company.name, balance + u" 元", 
+                    u"感谢您的支持，祝工作愉快"
+                )
 
     def get_all_records(self):
         return CashRecord.objects.all()
