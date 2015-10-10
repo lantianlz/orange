babaa9ce06173dfaab22d6d76e7faf161878dd29
simplejoy <lantian-lz@163.com>
2015-10-10 19:39:05 +0800

    美洽代码添加

--- www/tasks.py
+++ www/tasks.py
@@ -34,33 +34,3 @@ def async_change_profile_from_weixin(user, app_key, openid):
     UserBase().change_profile_from_weixin(user, app_key, openid)
 
 
-@task(queue='www_worker', name='www_worker.async_send_buy_success_template_msg_by_user_id')
-def async_send_buy_success_template_msg_by_user_id(user_id, name, remark, app_key=None):
-    """
-    @note: 发送洗车码购买成功模板消息
-    """
-    from www.weixin.interface import WeixinBase
-    from www.account.interface import ExternalTokenBase
-
-    openid = ExternalTokenBase().get_weixin_openid_by_user_id(user_id)
-    if openid:
-        errcode, errmsg = WeixinBase().send_buy_success_template_msg(openid=openid, name=name, remark=remark, app_key=app_key)
-        # errcode, errmsg = WeixinBase().send_buy_success_template_msg(openid="oNYsJj1eg4fnU4tKLvH-f2IXlxJ4", name=name, remark=remark, app_key="orange")
-        logging.error(u"%s: errcode is:%s, errmsg is:%s" % (str(datetime.datetime.now()), errcode, errmsg))
-
-
-@task(queue='www_worker', name='www_worker.async_send_use_order_code_template_msg_by_user_id')
-def async_send_use_order_code_template_msg_by_user_id(user_id, product_type, name, time, remark, app_key=None):
-    """
-    note: 发送洗车码成功使用模板消息
-    """
-    from www.weixin.interface import WeixinBase
-    from www.account.interface import ExternalTokenBase
-
-    openid = ExternalTokenBase().get_weixin_openid_by_user_id(user_id)
-    if openid:
-        errcode, errmsg = WeixinBase().send_use_order_code_template_msg(openid=openid, product_type=product_type, name=name,
-                                                                        time=time, remark=remark, app_key=app_key)
-        # errcode, errmsg = WeixinBase().send_use_order_code_template_msg(openid="oNYsJj1eg4fnU4tKLvH-f2IXlxJ4", product_type=product_type, name=name,
-        #                                                                time=time, remark=remark,  app_key="orange")
-        logging.error(u"%s: errcode is:%s, errmsg is:%s" % (str(datetime.datetime.now()), errcode, errmsg))
